1fce8cc6b619350a8d97ddc3e48142fa1599cd0c
Mihai Țălu <talumihai@gmail.com>
2025-04-27 20:30:39 +0300

    Add files via upload

---
+++ MVVM/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace HealthTrackerMVVM
+{
+    using System.Windows;
+    using HealthTrackerMVVM.Pages;
+    using HealthTrackerMVVM.Service;
+
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            NavigationService.Instance.Initialize(this.MainFrame);
+            NavigationService.Instance.NavigateTo(new AuthenticationPage(), this);
+        }
+    }
+}
