ff65c7012ec351a0131c8cb3fbcdff0e640ee52a
Mihai Țălu <talumihai@gmail.com>
2025-04-27 20:33:52 +0300

    Add files via upload

---
+++ MVVM/Utils/ApiClient.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthTrackerMVVM.Utils
+{
+    using System.Net.Http;
+
+    public class ApiClient
+    {
+        public static readonly HttpClient Client = new HttpClient();
+    }
+}
---
+++ MVVM/Utils/RelayCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace HealthTrackerMVVM.Utils
+{
+    using System.Windows.Input;
+
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> executeWithParam;
+        private readonly Action execute;
+        private readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action execute)
+        {
+            this.execute = execute ?? throw new ArgumentNullException(nameof(execute));
+        }
+
+        public RelayCommand(Action<object> executeWithParam, Predicate<object> canExecute = null)
+        {
+            this.executeWithParam = executeWithParam ?? throw new ArgumentNullException(nameof(executeWithParam));
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter) => this.canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object parameter)
+        {
+            if (this.execute != null)
+            {
+                this.execute();
+            }
+            else if (this.executeWithParam != null)
+            {
+                this.executeWithParam(parameter);
+            }
+        }
+    }
+
+}
